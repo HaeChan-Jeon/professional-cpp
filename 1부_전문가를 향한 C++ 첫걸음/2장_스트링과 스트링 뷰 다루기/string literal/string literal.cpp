49146cd747c039484c2096de1032e24e39cc4d0d
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-09-05 07:33:32 +0900

    STUDY: 스트링 리터널 2

--- 1부_전문가를 향한 C++ 첫걸음/2장_스트링과 스트링 뷰 다루기/string literal/string literal.cpp
+++ 1부_전문가를 향한 C++ 첫걸음/2장_스트링과 스트링 뷰 다루기/string literal/string literal.cpp
@@ -20,4 +20,21 @@ int main()
     const char* str1{ R"(Hello "World"!)" };
     cout << str1 << endl;
     cout << endl;
+
+    const char* str2{ "Line 1\nLine 2" };
+    cout << str2 << endl;
+    cout << endl;
+
+    const char* str3{ R"(Line 1
+Line 2)" };
+    cout << str3 << endl;
+    cout << endl;
+
+    const char* str4{ R"(Is the following a tab character? \t)" };
+    cout << str4 << endl;
+    cout << endl;
+
+    const char* str5{ R"-(Embedded )" characters)-" };
+    cout << str5 << endl;
+    cout << endl;
 }
