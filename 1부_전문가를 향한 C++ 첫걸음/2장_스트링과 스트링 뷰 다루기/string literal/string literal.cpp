56cd93e8a146c85f72e1bd26231e3ea6d44aa64c
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-09-04 16:58:22 +0900

    STUDY: 스트링 리터널 1

---
+++ 1부_전문가를 향한 C++ 첫걸음/2장_스트링과 스트링 뷰 다루기/string literal/string literal.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello" << endl;
+    cout << endl;
+
+    char arr[]{ "hello" };
+    arr[1] = 'a';
+    cout << arr << endl;
+    cout << endl;
+
+    // 로 스트링 리터널
+    const char* str{ "Hello \"World\"!" };
+    cout << str << endl;
+    cout << endl;
+
+    const char* str1{ R"(Hello "World"!)" };
+    cout << str1 << endl;
+    cout << endl;
+}
