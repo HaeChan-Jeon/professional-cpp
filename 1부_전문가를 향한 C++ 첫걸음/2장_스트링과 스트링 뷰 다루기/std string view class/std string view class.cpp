c49e32ed4b93a5d368c9f77d84680e2e934fa577
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-09-05 15:08:34 +0900

    STUDY: std::string_veiw class 및 포맷 지정자

---
+++ 1부_전문가를 향한 C++ 첫걸음/2장_스트링과 스트링 뷰 다루기/std string view class/std string view class.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string_view>
+#include <format>
+
+using namespace std;
+
+string_view extractExtension(string_view filename)
+{
+	return filename.substr(filename.rfind('.'));
+}
+
+int main()
+{
+	string filename{ R"(c;\temp\my file.ext)" };
+	cout << format("C++ string: {}", extractExtension(filename)) << endl;
+
+	const char* cString{ R"(c;\temp\my file.ext)" };
+	cout << format("C string: {}", extractExtension(cString)) << endl;
+
+	cout << format("Literal: {}", extractExtension(R"(c;\temp\my file.ext)")) << endl;
+
+	// std::string_view와 임시 스트링
+	string s{ "Hello" };
+	string_view sv{ s + " World!" };
+	cout << sv << endl;
+
+	// std::string_view 리터럴
+	auto sv2{ "My string_view"sv };
+
+	// 포맷 지정자
+	int i{ 42 };
+	cout << format("|{:5}|", i) << endl;
+	cout << format("|{:{}}|", i, 7) << endl;
+	cout << format("|{:7}|", i) << endl;
+	cout << format("|{:<7}|", i) << endl;
+	cout << format("|{:_>7}|", i) << endl;
+	cout << format("|{:_^7}|", i) << endl;
+
+	cout << format("|{:<5}|", i) << endl;
+	cout << format("|{:<+5}|", i) << endl;
+	cout << format("|{:< 5}|", i) << endl;
+	cout << format("|{:< 5}|", -i) << endl;
+}
