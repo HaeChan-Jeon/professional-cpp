c76454965071b308e3fa7c51a97ae63faea5f250
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-09-09 11:46:45 +0900

    STUDY: Handling dynamic memory 1

---
+++ 3부_전문가답게 C++ 코딩하기/7장_메모리 관리/Handling dynamic memory/Handling dynamic memory.cpp
@@ -0,0 +1,15 @@
+﻿int main()
+{
+	// 메모리의 작동 과정 살펴보기
+	int i{ 7 }; // 자동 변수, 스택
+
+	//int* ptr{ nullptr };
+	//ptr = new int; // 프리스토어 할당
+
+	int* ptr { new int };
+
+	// 포인트가 스택과 프리스토어에 모두 있는 예
+	int** handle{ nullptr };
+	handle = new int*;
+	*handle = new int;
+}
