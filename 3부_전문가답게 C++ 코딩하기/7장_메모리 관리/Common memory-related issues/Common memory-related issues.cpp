f07aaf72a6c3a6418c9efb821d3e5c9ae4af6335
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-09-10 11:36:32 +0900

    STUDY: common memory-related issues

---
+++ 3부_전문가답게 C++ 코딩하기/7장_메모리 관리/Common memory-related issues/Common memory-related issues.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+	#ifndef DBG_NEW
+		#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__)
+		#define new DBG_NEW
+	#endif
+#endif // _DEBUG
+
+#include <iostream>
+class Simple
+{
+public:
+	Simple() { m_intPtr = new int{}; }
+	~Simple() { delete m_intPtr;}
+	void setValue(int value) { *m_intPtr = value; }
+private:
+	int* m_intPtr;
+};
+
+void doSomething(Simple*& outSimplePtr)
+{
+	outSimplePtr = new Simple();
+}
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	Simple* simplePtr{ new Simple() };
+	doSomething(simplePtr);
+	delete simplePtr;
+}
