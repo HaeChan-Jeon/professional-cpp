e6d1b911ae16b871276c1d2767525de5b49b51c8
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-09-10 10:55:38 +0900

    STUDY: the two faces of arrays and pointers

---
+++ 3부_전문가답게 C++ 코딩하기/7장_메모리 관리/The Two Faces of Arrays and Pointers/The Two Faces of Arrays and Pointers.cpp
@@ -0,0 +1,26 @@
+﻿#include <array>;
+
+void doubleInts(int* theArray, size_t size)
+{
+	for (size_t i{ 0 }; i < size; i++) { theArray[i] *= 2; }
+}
+
+int main()
+{
+	// 배열 = 포인터
+	int myIntArray[10]{};
+	int* myIntPtr{ myIntArray };
+	//포인터로 배열 접근하기
+	myIntPtr[4] = 5;
+
+	size_t arrSize{ 4 };
+	int* freeStoreArray{ new int[arrSize] {1, 5, 3, 4} };
+	doubleInts(freeStoreArray, arrSize);
+	delete[] freeStoreArray;
+	freeStoreArray = nullptr;
+
+	int stackArray[]{ 1, 2, 3, 4 };
+	arrSize = std::size(stackArray);
+	doubleInts(stackArray, arrSize);
+	doubleInts(&stackArray[0], arrSize);
+}
