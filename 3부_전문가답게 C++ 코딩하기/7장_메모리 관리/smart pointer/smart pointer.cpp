ba02041c53566847f480a50cbc4b44653b03caad
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-09-11 16:39:15 +0900

    STUDY: unique_ptr 2

--- 3부_전문가답게 C++ 코딩하기/7장_메모리 관리/smart pointer/smart pointer.cpp
+++ 3부_전문가답게 C++ 코딩하기/7장_메모리 관리/smart pointer/smart pointer.cpp
@@ -34,9 +34,34 @@ void notLeaky()
 
 	unique_ptr<Simple> mySimpleSmartPtr2{ new Simple{} };
 	mySimpleSmartPtr2->go();
+
+	// unique_ptr 사용 방법
+	(*mySimpleSmartPtr1).go();
+
+	processData(mySimpleSmartPtr1.get());
+
+	mySimpleSmartPtr1.reset();				// 리소스 해제 후 nullptr로 초기화
+	mySimpleSmartPtr1.reset(new Simple());	// 리소스 해제 후 새로운 Simple 인스턴스로 설정
+
+	Simple* simple{ mySimpleSmartPtr1.release() };	// 소유권을 해제한다.
+	// simple 포인터를 사용하는 코드
+	simple->go();
+	delete simple;
+	simple = nullptr;
 }
 
+void processData(Simple* simple)
+{
+	// 스마트 포인터를 사용하는 코드
+}
+
+// unique_ptr과 c 스타일 배열
+auto myVariableSizedArray{ make_unique<int[]>(10) };
+
+// 커스텀 제거자
+int* my_alloc(int value) { return new int{ value }; }
 
 int main()
 {
+	myVariableSizedArray[1] = 123;
 }
