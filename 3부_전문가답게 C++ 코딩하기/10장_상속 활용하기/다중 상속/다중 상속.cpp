01538b2dbba05581079939e5017f660d668f9010
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-13 16:20:10 +0900

    STUDY: 여러 클래스 상속하기

---
+++ 3부_전문가답게 C++ 코딩하기/10장_상속 활용하기/다중 상속/다중 상속.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Dog
+{
+    public:
+        virtual void bark() { cout << "Woof!" << endl; }
+};
+
+class Bird
+{
+    public:
+        virtual void chirp() { cout << "Chirp!" << endl; }
+};
+
+class DogBird : public Dog, public Bird
+{
+};
+
+int main()
+{
+    DogBird myConfusedAnimal;
+    myConfusedAnimal.bark();
+    myConfusedAnimal.chirp();
+}
