ab1b9dc2514e402c528adbe3fe51671f3fb65312
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-13 16:24:13 +0900

    STUDY: 모호한 이름

--- 3부_전문가답게 C++ 코딩하기/10장_상속 활용하기/다중 상속/다중 상속.cpp
+++ 3부_전문가답게 C++ 코딩하기/10장_상속 활용하기/다중 상속/다중 상속.cpp
@@ -6,21 +6,30 @@ class Dog
 {
     public:
         virtual void bark() { cout << "Woof!" << endl; }
+        virtual void eat() { cout << "The dog ate." << endl; }
 };
 
 class Bird
 {
     public:
         virtual void chirp() { cout << "Chirp!" << endl; }
+        virtual void eat() { cout << "The bird ate." << endl; }
 };
 
 class DogBird : public Dog, public Bird
 {
+public:
+    void eat() override
+    {
+        Dog::eat();
+    }
+    using Dog::eat;
 };
 
 int main()
 {
     DogBird myConfusedAnimal;
-    myConfusedAnimal.bark();
-    myConfusedAnimal.chirp();
+    dynamic_cast<Dog&>(myConfusedAnimal).eat();
+    myConfusedAnimal.Dog::eat();
+    myConfusedAnimal.eat();
 }
