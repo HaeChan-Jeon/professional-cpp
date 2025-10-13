49b758a7f4e83382b98e806aa482e75ec2c14bea
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-13 11:04:24 +0900

    STUDY: 부모 클래스의 소멸자

--- 3부_전문가답게 C++ 코딩하기/10장_상속 활용하기/10.3 부모를 공경하라/10.3 부모를 공경하라.cpp
+++ 3부_전문가답게 C++ 코딩하기/10장_상속 활용하기/10.3 부모를 공경하라/10.3 부모를 공경하라.cpp
@@ -6,24 +6,31 @@ class Something
 {
     public:
         Something() { cout << "2"; }
+        //virtual ~Something() { cout << "2"; }
+        ~Something() { cout << "2"; }
 };
 
 class Base
 {
     public:
-        //Base() { cout << "1"; }
-        Base(int i) { cout << i; };
+        Base() { cout << "1"; }
+        //virtual ~Base() { cout << "1"; }
+        ~Base() { cout << "1"; }
 };
 
 class Derived : public Base
 {
     public:
-        Derived() : Base { 7 } { cout << "3"; }
-    //private:
-    //    Something m_dataMember;
+        Derived() { cout << "3"; }
+        //virtual ~Derived() { cout << "3"; }
+        ~Derived() { cout << "3"; }
+    private:
+        Something m_dataMember;
 };
 
 int main()
 {
-    Derived myDerived;
+    //Derived myDerived;
+    Base* ptr{ new Derived{} };
+    delete ptr;
 }
