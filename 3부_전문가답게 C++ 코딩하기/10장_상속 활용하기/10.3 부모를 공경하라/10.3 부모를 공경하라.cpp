0e29f98d9fe29bcf7634b52033e13b84852183ee
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-13 10:59:19 +0900

    STUDY: 부모 클래스의 생성자

---
+++ 3부_전문가답게 C++ 코딩하기/10장_상속 활용하기/10.3 부모를 공경하라/10.3 부모를 공경하라.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Something
+{
+    public:
+        Something() { cout << "2"; }
+};
+
+class Base
+{
+    public:
+        //Base() { cout << "1"; }
+        Base(int i) { cout << i; };
+};
+
+class Derived : public Base
+{
+    public:
+        Derived() : Base { 7 } { cout << "3"; }
+    //private:
+    //    Something m_dataMember;
+};
+
+int main()
+{
+    Derived myDerived;
+}
