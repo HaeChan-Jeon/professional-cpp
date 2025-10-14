192968f5936bc46289c1f4b1c3e147cde3646c02
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-14 16:05:02 +0900

    STUDY: static_cast() 2

--- 3부_전문가답게 C++ 코딩하기/10장_상속 활용하기/캐스트/캐스트.cpp
+++ 3부_전문가답게 C++ 코딩하기/10장_상속 활용하기/캐스트/캐스트.cpp
@@ -1,9 +1,26 @@
 ﻿#include <iostream>
 
+class Base
+{
+	public:
+		virtual ~Base() = default;
+};
+
+class Derived : public Base
+{
+	public:
+		virtual ~Derived() = default;
+};
+
 int main()
 {
-	int i{ 3 };
-	int j{ 4 };
-	double result{ static_cast<double>(i) / j };
-	std::cout << result;
+	Base* b{ nullptr };
+	Derived* d{ new Derived{} };
+	b = d; // 상속 계층의 상위 타입으로 업캐스트할 필요 없다.
+	d = static_cast<Derived*>(b); // 상속 계층의 하위 타입으로 다운캐스트해야 한다.
+
+	Base base;
+	Derived derived;
+	Base& br{ derived };
+	Derived& dr{ static_cast<Derived&>(br) };
 }
