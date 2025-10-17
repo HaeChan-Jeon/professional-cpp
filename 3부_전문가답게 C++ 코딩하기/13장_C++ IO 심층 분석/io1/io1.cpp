ef07772abfb0ff93e835a0654f7f2f77324da524
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-17 11:09:09 +0900

    STUDY: 출력 에러 처리하기

--- 3부_전문가답게 C++ 코딩하기/13장_C++ IO 심층 분석/io1/io1.cpp
+++ 3부_전문가답게 C++ 코딩하기/13장_C++ IO 심층 분석/io1/io1.cpp
@@ -13,4 +13,29 @@ int main()
 	cout.flush();
 	cout << "def";
 	cout << endl;
+
+	if (cout.good()) {
+		cout << "All good\n" << endl;
+	}
+
+	cout.flush();
+	if (cout.fail()) {
+		cerr << "Unable to flush to standard out\n" << endl;
+	}
+
+	cout.flush();
+	if (!cout) {
+		cerr << "Unable to flush to standard out\n" << endl;
+	}
+
+	cout.exceptions(ios::failbit | ios::badbit | ios::eofbit);
+	try	{
+		cout << "Hello World." << endl;
+	}
+	catch (const ios_base::failure& ex) {
+		cerr << "Caught exception: " << ex.what()
+			<< ", error code = " << ex.code() << endl;
+	}
+
+	cout.clear();
 }
