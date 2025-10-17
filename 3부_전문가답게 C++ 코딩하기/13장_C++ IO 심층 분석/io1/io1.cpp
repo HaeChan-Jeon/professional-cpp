ba8994318083da5c4d4740ad802c290386fd1656
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-17 11:03:34 +0900

    STUDY: 출력 스트림에서 제공하는 메서드

---
+++ 3부_전문가답게 C++ 코딩하기/13장_C++ IO 심층 분석/io1/io1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const char* test{ "hello, there\n" };
+	cout.write(test, strlen(test));
+
+	cout.put('a');
+
+	cout << "abc";
+	cout.flush();
+	cout << "def";
+	cout << endl;
+}
