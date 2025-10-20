ba0ef111838db6fabdf7a02c5da54675b951f95e
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-20 11:33:10 +0900

    STUDY: 중첩 익셉션

--- 3부_전문가답게 C++ 코딩하기/14장_에러 처리/exception1/exception1.cpp
+++ 3부_전문가답게 C++ 코딩하기/14장_에러 처리/exception1/exception1.cpp
@@ -8,21 +8,23 @@ using namespace std;
 class MyException : public exception
 {
 public:
-	MyException(string message,
-		source_location location = source_location::current())
-		: m_message{ move(message) }
-		, m_location{ move(location) }
-	{ }
+	MyException(string_view message) : m_message { move(message)} {}
 	const char* what() const noexcept override { return m_message.c_str(); }
-	virtual const source_location& where() const noexcept { return m_location; }
 private:
 	string m_message;
-	source_location m_location;
 };
 
 void doSomeThing()
 {
-	throw MyException{ "Throwing MyException." };
+	try	{
+		throw runtime_error{ "Throwing a runtime_error exception" };
+	}
+	catch (const runtime_error& e) {
+		cout << format("{} caught a runtime_error", __func__) << endl;
+		cout << format("{} throwing MyException", __func__) << endl;
+		throw_with_nested(
+			MyException{ "MyException with nested runtime_error" });
+	};
 }
 
 int main()
@@ -30,8 +32,11 @@ int main()
 	try {
 		doSomeThing();
 	} catch (const MyException& e) {
-		const auto& location{ e.where()};
-		cerr << format("Caught: '{}' at line {} in {}.",
-			e.what(), location.line(), location.function_name()) << endl;
+		cout << format("{} Caught MyException: {}", __func__, e.what()) << endl;
+		try {
+			rethrow_if_nested(e);
+		} catch (const runtime_error& e) {
+		cout << format(" Nested exception: {}", e.what()) << endl;
+		}
 	}
 }
