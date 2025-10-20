1c5e23f3a095ec630bb3b368d84ee350aa4665f4
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-20 11:11:56 +0900

    STUDY: 로그에 소스 위치 남기기

---
+++ 3부_전문가답게 C++ 코딩하기/14장_에러 처리/exception1/exception1.cpp
@@ -0,0 +1,23 @@
+﻿#include <source_location>
+#include <string>
+#include <format>
+#include <iostream>
+
+using namespace std;
+
+void logMessage(string_view message,
+	const source_location& location = source_location::current())
+{
+	cout << format("{}({}): {}: {}", location.file_name(),
+		location.line(), location.function_name(), message) << endl;
+}
+
+void foo()
+{
+	logMessage("Starting execution of foo().");
+}
+
+int main()
+{
+	foo();
+}
