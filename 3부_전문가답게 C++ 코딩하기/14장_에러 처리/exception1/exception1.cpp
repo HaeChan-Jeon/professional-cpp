f83bfeaaedf15d21a7108a24c9e76ed61d622952
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-20 11:19:55 +0900

    STUDY: 익셉션에 소스 위치 담기

--- 3부_전문가답게 C++ 코딩하기/14장_에러 처리/exception1/exception1.cpp
+++ 3부_전문가답게 C++ 코딩하기/14장_에러 처리/exception1/exception1.cpp
@@ -5,19 +5,33 @@
 
 using namespace std;
 
-void logMessage(string_view message,
-	const source_location& location = source_location::current())
+class MyException : public exception
 {
-	cout << format("{}({}): {}: {}", location.file_name(),
-		location.line(), location.function_name(), message) << endl;
-}
+public:
+	MyException(string message,
+		source_location location = source_location::current())
+		: m_message{ move(message) }
+		, m_location{ move(location) }
+	{ }
+	const char* what() const noexcept override { return m_message.c_str(); }
+	virtual const source_location& where() const noexcept { return m_location; }
+private:
+	string m_message;
+	source_location m_location;
+};
 
-void foo()
+void doSomeThing()
 {
-	logMessage("Starting execution of foo().");
+	throw MyException{ "Throwing MyException." };
 }
 
 int main()
 {
-	foo();
+	try {
+		doSomeThing();
+	} catch (const MyException& e) {
+		const auto& location{ e.where()};
+		cerr << format("Caught: '{}' at line {} in {}.",
+			e.what(), location.line(), location.function_name()) << endl;
+	}
 }
