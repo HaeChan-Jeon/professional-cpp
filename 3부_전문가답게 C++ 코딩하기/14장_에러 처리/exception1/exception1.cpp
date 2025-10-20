ff2c494b2f8fce956213a6c186e652594db6dcc9
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-20 11:43:19 +0900

    STUDY: 스택 풀기와 정리

--- 3부_전문가답게 C++ 코딩하기/14장_에러 처리/exception1/exception1.cpp
+++ 3부_전문가답게 C++ 코딩하기/14장_에러 처리/exception1/exception1.cpp
@@ -1,42 +1,34 @@
-﻿#include <source_location>
-#include <string>
-#include <format>
+﻿#include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-class MyException : public exception
-{
-public:
-	MyException(string_view message) : m_message { move(message)} {}
-	const char* what() const noexcept override { return m_message.c_str(); }
-private:
-	string m_message;
-};
-
-void doSomeThing()
-{
-	try	{
-		throw runtime_error{ "Throwing a runtime_error exception" };
-	}
-	catch (const runtime_error& e) {
-		cout << format("{} caught a runtime_error", __func__) << endl;
-		cout << format("{} throwing MyException", __func__) << endl;
-		throw_with_nested(
-			MyException{ "MyException with nested runtime_error" });
-	};
-}
+void funcOne();
+void funcTwo();
 
 int main()
 {
-	try {
-		doSomeThing();
-	} catch (const MyException& e) {
-		cout << format("{} Caught MyException: {}", __func__, e.what()) << endl;
-		try {
-			rethrow_if_nested(e);
-		} catch (const runtime_error& e) {
-		cout << format(" Nested exception: {}", e.what()) << endl;
-		}
+	try	{
+		funcOne();
+	} catch (const exception& e) {
+		cerr << "Exception cought!" << endl;
+		return 1;
 	}
+}
+
+void funcOne()
+{
+	string str1;
+	string* str2{ new string {} };
+	funcTwo();
+	delete str2;
+}
+
+void funcTwo()
+{
+	ifstream fileStream;
+	fileStream.open("filename");
+	throw exception{};
+	fileStream.close();
 }
