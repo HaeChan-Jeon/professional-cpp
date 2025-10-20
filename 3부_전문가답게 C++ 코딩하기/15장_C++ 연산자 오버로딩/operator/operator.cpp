b626250b465e86d3912b065af3fd050aa1a1a6d5
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-20 15:31:33 +0900

    STUDY: 함수 호출 연산자 오버로딩하기

---
+++ 3부_전문가답게 C++ 코딩하기/15장_C++ 연산자 오버로딩/operator/operator.cpp
@@ -0,0 +1,24 @@
+﻿class FunctionObject
+{
+    public:
+        int operator() (int param);
+        int doSquare(int param);
+};
+
+int FunctionObject::operator() (int param)
+{
+    return doSquare(param);
+}
+
+int FunctionObject::doSquare(int param)
+{
+    return param * param;
+}
+
+int main()
+{
+    int x{ 3 }, xSquared, xSquaredAgain;
+    FunctionObject square;
+    xSquared = square(x);
+    xSquaredAgain = square.doSquare(x);
+}
