d7479de2bd280579cf26c3b4876ed0d2678cebd8
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-21 14:47:01 +0900

    STUDY: 고정 크기 vector

---
+++ 3부_전문가답게 C++ 코딩하기/18장_표준 라이브러리 컨테이너/vector/vector.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+#include <iostream>
+#include <format>
+
+using namespace std;
+
+int main()
+{
+	vector<double> doubleVector(10);
+
+	double max{ -numeric_limits<double>::infinity() };
+
+	for (size_t i{ 0 }; i < doubleVector.size(); i++) {
+		cout << format("Enter score {}: ", i + 1);
+		cin >> doubleVector[i];
+		if (doubleVector[i] > max) {
+			max = doubleVector[i];
+		}
+	}
+
+	max /= 100.0;
+	for (auto& element : doubleVector) {
+		element /= max;
+		cout << element << " ";
+	}
+}
