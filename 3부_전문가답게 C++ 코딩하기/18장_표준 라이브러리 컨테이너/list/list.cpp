1efd8ef71b3d3ffaf6e81acd01f8c8e21fa566a8
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-22 11:25:02 +0900

    STUDY: splice()

---
+++ 3부_전문가답게 C++ 코딩하기/18장_표준 라이브러리 컨테이너/list/list.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    list<string> dictionary{ "aardvark", "ambulance" };
+    list<string> bWords{ "bathos", "balderdash" };
+    dictionary.push_back("canticle");
+    dictionary.push_back("consumerism");
+
+    if (!bWords.empty()) {
+        auto iterLastB{ --(cend(bWords)) };
+        auto it{ cbegin(dictionary) };
+        for (; it != cend(dictionary); ++it) {
+            if (*it > *iterLastB) {
+                break;
+            }
+        }
+        dictionary.splice(it, bWords);
+    }
+
+    for (const auto& word : dictionary) {
+        cout << word << endl;
+    }
+}
