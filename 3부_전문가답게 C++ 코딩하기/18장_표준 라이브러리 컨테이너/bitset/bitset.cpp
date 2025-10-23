8e081528ffd7fa891b389a51d86df54cb97675da
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-23 14:29:41 +0900

    STUDY: 비트 연산

--- 3부_전문가답게 C++ 코딩하기/18장_표준 라이브러리 컨테이너/bitset/bitset.cpp
+++ 3부_전문가답게 C++ 코딩하기/18장_표준 라이브러리 컨테이너/bitset/bitset.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 int main()
 {
-	bitset<10> myBitset;
+	auto str1{ "0011001100" };
+	auto str2{ "0000111100" };
+	bitset<10> bitsOne{ str1 };
+	bitset<10> bitsTwo{ str2 };
 
-	myBitset.set(3);
-	myBitset.set(6);
-	myBitset[8] = true;
-	myBitset[9] = myBitset[3];
-
-	if (myBitset.test(3)) {
-		cout << "Bit 3 is set!" << endl;
-	}
-	cout << myBitset << endl;
+	auto bitsThree{ bitsOne & bitsTwo };
+	cout << bitsThree << endl;
+	bitsThree <<= 4;
+	cout << bitsThree << endl;
 }
