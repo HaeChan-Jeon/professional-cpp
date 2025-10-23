501377d69945000152ccbecd9c8f816eb37c9546
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-23 13:45:10 +0900

    STUDY: set 사용 예제: 접근 권한 관리(AccessList)

---
+++ 3부_전문가답게 C++ 코딩하기/18장_표준 라이브러리 컨테이너/set/set.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class AccessList final
+{
+public:
+    AccessList() = default;
+
+    AccessList(std::initializer_list<std::string_view> users)
+    {
+        m_allowed.insert(begin(users), end(users));
+    }
+
+    void addUser(std::string user)
+    {
+        m_allowed.emplace(std::move(user));
+    }
+
+    void removeUser(const std::string& user)
+    {
+        m_allowed.erase(user);
+    }
+
+    bool isAllowed(const std::string& user) const
+    {
+        return (m_allowed.count(user) != 0);
+    }
+    
+    std::vector<std::string> getAllUsers() const
+    {
+        return { begin(m_allowed), end(m_allowed) };
+    }
+private:
+    std::set<std::string> m_allowed;
+};
+
+int main()
+{
+    AccessList fileX{ "mgregoire", "baduser" };
+    fileX.addUser("pvw");
+    fileX.removeUser("baduser");
+
+    if (fileX.isAllowed("mgregoire")) {
+        cout << "mgregoire has permissions" << endl;
+    }
+
+    if (fileX.isAllowed("baduser")) {
+        cout << "baduser has permissions" << endl;
+    }
+
+    auto users{ fileX.getAllUsers() };
+    for (const auto& user : users) {
+        cout << user << " ";
+    }
+}
