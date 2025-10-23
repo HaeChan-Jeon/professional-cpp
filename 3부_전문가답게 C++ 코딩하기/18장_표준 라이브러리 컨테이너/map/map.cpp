d271b0258843cf96e195ec8f872ba0fd16ad19c6
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-23 11:40:31 +0900

    STUDY: map

---
+++ 3부_전문가답게 C++ 코딩하기/18장_표준 라이브러리 컨테이너/map/map.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	map<int, int> intMap;
+
+	if (auto result {intMap.insert({1, 4})}; result.second) {
+		cout << "Insert succeeded!" << endl;
+	}
+
+	if (auto [iter, success] { intMap.insert({2, 5}) }; success) {
+		cout << "Insert succeeded!" << endl;
+	}
+
+	auto ret = intMap.insert_or_assign(1, 5);
+	if (ret.second) {
+		cout << "Inserted." << endl;
+	} else {
+		cout << "Overwritten." << endl;
+	}
+
+	for (const auto& p : intMap ) {
+		cout << p.second << endl;
+	}
+	
+	auto it{ intMap.find(1) };
+	if (it != end(intMap)) {
+		it -> second = 100;
+	}
+
+	auto isKeyInMap{ intMap.contains(1) };
+
+	map<int, int> intMap2;
+	auto extractedNode{ intMap.extract(1) };
+	intMap2.insert(move(extractedNode));
+
+	intMap2.insert(intMap.extract(2));
+
+	for (const auto& p : intMap) {
+		cout << p.second << endl;
+	}
+
+	intMap.merge(intMap2);
+
+	for (const auto& p : intMap) {
+		cout << p.second << endl;
+	}
+}
