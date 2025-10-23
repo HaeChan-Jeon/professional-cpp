ed66e7b90d5159aa2fcbead266231955369f9f83
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-23 11:20:10 +0900

    STUDY: pair

---
+++ 3부_전문가답게 C++ 코딩하기/18장_표준 라이브러리 컨테이너/pair/pair.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+	pair<string, int> myPair{ "hello", 5 };
+	pair<string, int> myOtherPair;
+
+	myOtherPair.first = "hello";
+	myOtherPair.second = 6;
+
+	pair<string, int> myThirdPair{ myOtherPair };
+
+	pair myPair2{ "hello"s, 5 };
+
+	pair<int, double> pair1{ make_pair(5, 10.10) };
+	auto pair2{ make_pair(5, 10.10) };
+	pair pair3{ 5, 10.10 };
+}
