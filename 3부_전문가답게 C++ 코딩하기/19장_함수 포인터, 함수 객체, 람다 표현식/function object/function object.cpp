be627b6af884b0b98aea6a0a5bd21810210061f1
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-24 14:47:37 +0900

    STUDY: 산술 함수 객체

--- 3부_전문가답게 C++ 코딩하기/19장_함수 포인터, 함수 객체, 람다 표현식/function object/function object.cpp
+++ 3부_전문가답게 C++ 코딩하기/19장_함수 포인터, 함수 객체, 람다 표현식/function object/function object.cpp
@@ -3,48 +3,69 @@
 #include <span>
 #include <format>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-using Matcher = function<bool(int, int)>;
+//using Matcher = function<bool(int, int)>;
+//
+//using MatchHandler = function<void(size_t, int, int)>;
+//
+//bool intEqual(int item1, int item2) { return item1 == item2; }
+//
+//void printMatch(size_t position, int value1, int value2)
+//{
+//	cout << format("Match found at position {} ({}, {})",
+//		position, value1, value2) << endl;
+//}
+//
+//template<typename Matcher, typename MatchHandler>
+//void findMatches(span<const int> values1, span<const int> values2,
+//	Matcher matcher, MatchHandler handler)
+//{
+//	if (values1.size() != values2.size()) { return; }
+//
+//	for (size_t i{ 0 }; i < values1.size(); ++i) {
+//		if (matcher(values1[i], values2[i])) {
+//			handler(i, values1[i], values2[i]);
+//		}
+//	}
+//}
+//
+//class IsLargerThan
+//{
+//public:
+//    explicit IsLargerThan(int value) : m_value{ value } {}
+//    bool operator()(int value1, int value2) const {
+//        return value1 > m_value && value2 > m_value;
+//    }
+//private:
+//    int m_value;
+//};
 
-using MatchHandler = function<void(size_t, int, int)>;
-
-bool intEqual(int item1, int item2) { return item1 == item2; }
-
-void printMatch(size_t position, int value1, int value2)
+template <typename Iter, typename StartValue, typename Operation>
+auto accumulateData(Iter begin, Iter end, StartValue startValue, Operation op)
 {
-	cout << format("Match found at position {} ({}, {})",
-		position, value1, value2) << endl;
-}
-
-template<typename Matcher, typename MatchHandler>
-void findMatches(span<const int> values1, span<const int> values2,
-	Matcher matcher, MatchHandler handler)
-{
-	if (values1.size() != values2.size()) { return; }
-
-	for (size_t i{ 0 }; i < values1.size(); ++i) {
-		if (matcher(values1[i], values2[i])) {
-			handler(i, values1[i], values2[i]);
-		}
+	auto accumulated{ startValue };
+	for (Iter iter{ begin }; iter != end; ++iter) {
+		accumulated = op(accumulated, *iter);
 	}
+	return accumulated;
 }
 
-class IsLargerThan
+double geometricMean(span<const int> values)
 {
-public:
-    explicit IsLargerThan(int value) : m_value{ value } {}
-    bool operator()(int value1, int value2) const {
-        return value1 > m_value && value2 > m_value;
-    }
-private:
-    int m_value;
-};
+	auto mult{ accumulateData(cbegin(values), cend(values), 1, multiplies<int>{}) };
+	return pow(mult, 1.0 / values.size());
+}
 
 int main()
 {
-	vector values1{ 2, 500, 6, 9, 10, 101, 1 };
-	vector values2{ 4, 4, 2, 9, 0, 300, 1 };
-	findMatches(values1, values2, IsLargerThan { 100 }, &printMatch);
+	//vector values1{ 2, 500, 6, 9, 10, 101, 1 };
+	//vector values2{ 4, 4, 2, 9, 0, 300, 1 };
+	//findMatches(values1, values2, IsLargerThan { 100 }, &printMatch);
+
+	plus<int> myPlus;
+	int res{ myPlus(4, 5) };
+	cout << res << endl;
 }
