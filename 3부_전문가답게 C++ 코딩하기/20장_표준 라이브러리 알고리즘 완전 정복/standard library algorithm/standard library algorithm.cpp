bcc07cce6e3edf1a12cab363e374ec2fd782899c
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-27 15:25:28 +0900

    STUDY: 최대/최소 알고리즘

--- 3부_전문가답게 C++ 코딩하기/20장_표준 라이브러리 알고리즘 완전 정복/standard library algorithm/standard library algorithm.cpp
+++ 3부_전문가답게 C++ 코딩하기/20장_표준 라이브러리 알고리즘 완전 정복/standard library algorithm/standard library algorithm.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <format>
 
 using namespace std;
 
@@ -27,21 +28,26 @@ void DumpRange(string_view message, Iterator begin, Iterator end)
 
 int main()
 {
-	vector<int> vectorOne, vectorTwo, vectorMerged;
-	cout << "Enter values for first vector:" << endl;
-	polulateContainter(vectorOne);
-	cout << "Enter values for second vector:" << endl;
-	polulateContainter(vectorTwo);
+	int x{ 4 }, y{ 5 };
+	cout << format("x is {} and y is {}", x, y) << endl;
+	cout << "Max is " << max(x, y) << endl;
+	cout << "Min is " << min(x, y) << endl;
 
-	// 두 컨테이너를 정렬한다.
-	sort(begin(vectorOne), end(vectorOne));
-	sort(begin(vectorTwo), end(vectorTwo));
+	// 두 개 이상의 값에 대해 max()와 min()을 구하는 예
+	int x1{ 2 }, x2{ 9 }, x3{ 3 }, x4{ 12 };
+	cout << "Max of 4 elements is " << max({ x1, x2, x3, x4 }) << endl;
+	cout << "Min of 4 elements is " << min({ x1, x2, x3, x4 }) << endl;
 
-	// 두 원본 vector 합친 값을 모두 담을 정도로 대상 vector의 크기를 넉넉히 확보한다.
-	vectorMerged.resize(size(vectorOne) + size(vectorTwo));
+	// minmax()를 구하는 예
+	auto p2{ minmax({x1, x2, x3, x4}) };
+	cout << format("Minmax of 4 elements is <{}, {}>", p2.first, p2.second) << endl;
 
-	merge(cbegin(vectorOne), cend(vectorOne),
-		cbegin(vectorTwo), cend(vectorTwo), begin(vectorMerged));
+	// minmax()와 C++17부터 지원하는 구조적 바인딩을 적용하는 예
+	auto[min1, max1] { minmax({x1, x2, x3, x4}) };
+	cout << format("Minmax of 4 elements is <{}, {}>", min1, max1) << endl;
 
-	DumpRange("Merged vector: ", cbegin(vectorMerged), cend(vectorMerged));
+	// minmax_element()와 C++17부터 지원하는 구조적 바인딩을 적용하는 예
+	vector values{ 11, 22, 33 };
+	auto [min2, max2] { minmax_element(cbegin(values), cend(values)) };
+	cout << format("Minmax_element() result: <{}, {}>", *min2, *max2) << endl;
 }
