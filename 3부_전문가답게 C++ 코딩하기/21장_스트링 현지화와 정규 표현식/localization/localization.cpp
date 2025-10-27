6e4e8c343d86d58595b1a9461a7b89eaec1bb1b4
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-28 08:28:59 +0900

    STUDY: 스트링 리터널 현지화하기

--- 3부_전문가답게 C++ 코딩하기/21장_스트링 현지화와 정규 표현식/localization/localization.cpp
+++ 3부_전문가답게 C++ 코딩하기/21장_스트링 현지화와 정규 표현식/localization/localization.cpp
@@ -1,9 +1,17 @@
 ﻿#include <iostream>
+#include <format>
 
 using namespace std;
 
 int main()
 {
+	// 확장 문자
 	wchar_t myWideCharacter{ L'm' };
 	wcout << L"I am a wide-character string Literal." << endl;
+
+	// 스트링 리터널 현지화하기
+	size_t n{ 5 };
+	wstring filename{ L"file1.txt" };
+	wcout << L"Read " << n << L" bytes from " << filename << endl;
+	cout << format(loadResource(IDS_TRANSFERRED), n, filename) << endl;
 }
