2545f3a274bc8a8cd0dc0ffa7907b1aa2ef836a4
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-27 15:35:48 +0900

    STUDY: 병렬 알고리즘

--- 3부_전문가답게 C++ 코딩하기/20장_표준 라이브러리 알고리즘 완전 정복/standard library algorithm/standard library algorithm.cpp
+++ 3부_전문가답게 C++ 코딩하기/20장_표준 라이브러리 알고리즘 완전 정복/standard library algorithm/standard library algorithm.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <format>
+#include <execution>
 
 using namespace std;
 
@@ -28,26 +29,6 @@ void DumpRange(string_view message, Iterator begin, Iterator end)
 
 int main()
 {
-	int x{ 4 }, y{ 5 };
-	cout << format("x is {} and y is {}", x, y) << endl;
-	cout << "Max is " << max(x, y) << endl;
-	cout << "Min is " << min(x, y) << endl;
-
-	// 두 개 이상의 값에 대해 max()와 min()을 구하는 예
-	int x1{ 2 }, x2{ 9 }, x3{ 3 }, x4{ 12 };
-	cout << "Max of 4 elements is " << max({ x1, x2, x3, x4 }) << endl;
-	cout << "Min of 4 elements is " << min({ x1, x2, x3, x4 }) << endl;
-
-	// minmax()를 구하는 예
-	auto p2{ minmax({x1, x2, x3, x4}) };
-	cout << format("Minmax of 4 elements is <{}, {}>", p2.first, p2.second) << endl;
-
-	// minmax()와 C++17부터 지원하는 구조적 바인딩을 적용하는 예
-	auto[min1, max1] { minmax({x1, x2, x3, x4}) };
-	cout << format("Minmax of 4 elements is <{}, {}>", min1, max1) << endl;
-
-	// minmax_element()와 C++17부터 지원하는 구조적 바인딩을 적용하는 예
-	vector values{ 11, 22, 33 };
-	auto [min2, max2] { minmax_element(cbegin(values), cend(values)) };
-	cout << format("Minmax_element() result: <{}, {}>", *min2, *max2) << endl;
+	vector<int> myVector;
+	sort(execution::par, begin(myVector), end(myVector));
 }
