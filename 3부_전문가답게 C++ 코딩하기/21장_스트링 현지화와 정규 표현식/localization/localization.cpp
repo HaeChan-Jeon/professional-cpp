af2ae7ec5bb475fe1842132badbc17ff0f79101d
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-28 08:53:51 +0900

    STUDY: 문자 분류

--- 3부_전문가답게 C++ 코딩하기/21장_스트링 현지화와 정규 표현식/localization/localization.cpp
+++ 3부_전문가답게 C++ 코딩하기/21장_스트링 현지화와 정규 표현식/localization/localization.cpp
@@ -5,16 +5,7 @@
 
 using namespace std;
 
-void print()
-{
-	stringstream stream;
-	stream << 32767;
-	cout << stream.str() << endl;
-}
-
 int main()
 {
-	print();
-	locale::global(locale{ "en-US" });
-	print();
+	bool result{ isupper('E', locale {"fr-FR"}) };
 }
