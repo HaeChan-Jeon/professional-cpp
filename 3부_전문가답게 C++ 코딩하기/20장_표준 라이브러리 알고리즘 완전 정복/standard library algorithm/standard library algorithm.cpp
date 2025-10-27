7a53bb6ec11c7a4ca44591b8e7d95d5bca02f95d
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-27 10:44:39 +0900

    STUDY: accumulate() 알고리즘

---
+++ 3부_전문가답게 C++ 코딩하기/20장_표준 라이브러리 알고리즘 완전 정복/standard library algorithm/standard library algorithm.cpp
@@ -0,0 +1,29 @@
+﻿#include <numeric>
+#include <span>
+
+using namespace std;
+
+int product(int value1, int value2) { return value1 * value2; }
+
+double geometricMean(span<const int> values)
+{
+	int mult{ accumulate(cbegin(values), cend(values), 1, product) };
+	return pow(mult, 1.0 / values.size());
+}
+
+double geometricMeanLamda(span<const int> values)
+{
+	int mult{ accumulate(cbegin(values), cend(values), 1,
+		[](int value1, int value2) { return value1 * value2; }) };
+	return pow(mult, 1.0 / values.size());
+}
+
+double geometricMeanFunctor(span<const int> values)
+{
+	int mult{ accumulate(cbegin(values), cend(values), 1, multiplies<>{}) };
+	return pow(mult, 1.0 / values.size());
+}
+
+int main()
+{
+}
