2a3bca4b2b333cd33ac6c5dcccd03f6e92a2bb45
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-27 15:13:50 +0900

    STUDY: 집합 알고리즘

--- 3부_전문가답게 C++ 코딩하기/20장_표준 라이브러리 알고리즘 완전 정복/standard library algorithm/standard library algorithm.cpp
+++ 3부_전문가답게 C++ 코딩하기/20장_표준 라이브러리 알고리즘 완전 정복/standard library algorithm/standard library algorithm.cpp
@@ -17,41 +17,50 @@ void polulateContainter(Container& cont)
 	}
 }
 
+template <typename Iterator>
+void DumpRange(string_view message, Iterator begin, Iterator end)
+{
+	cout << message;
+	for_each(begin, end, [](const auto& element) { cout << element << " "; });
+	cout << endl;
+}
+
 int main()
 {
-	vector<int> values;
-	polulateContainter(values);
+	vector<int> vec1, vec2, result;
+	cout << "Enter elements for set 1:" << endl;
+	polulateContainter(vec1);
+	cout << "Enter elements for set 2:" << endl;
+	polulateContainter(vec2);
 
-	sort(begin(values), end(values));
+	// 정렬된 범위에 집합 알고리즘 적용하기
+	sort(begin(vec1), end(vec1));
+	sort(begin(vec2), end(vec2));
 
-	//cout << "Sorted vector: ";
-	//for (const auto& i : values) { cout << i << " "; }
-	//cout << endl;
+	DumpRange("Set 1: ", cbegin(vec1), cend(vec1));
+	DumpRange("Set 2: ", cbegin(vec2), cend(vec2));
 
-	//while (true) {
-	//	int number;
-	//	cout << "Enter a number to insert (0 th quit): ";
-	//	cin >> number;
-	//	if (number == 0) { break; }
-
-	//	auto iter{ lower_bound(begin(values), end(values), number) };
-	//	values.insert(iter, number);
-
-	//	cout << "New vector: ";
-	//	for (const auto& i : values) { cout << i << " "; }
-	//	cout << endl;
-	//}
-
-	while (true) {
-		int number;
-		cout << "Enter a number to insert (0 th quit): ";
-		cin >> number;
-		if (number == 0) { break; }
-
-		if (binary_search(cbegin(values), cend(values), number)) {
-			cout << "That number is in the vector." << endl;
-		} else {
-			cout << "That number is not in the vector." << endl;
-		}
+	if (includes(cbegin(vec1), cend(vec1), cbegin(vec2), cend(vec2))) {
+		cout << "The second set is a subset of the first." << endl;
 	}
+	if (includes(cbegin(vec2), cend(vec2), cbegin(vec1), cend(vec1))) {
+		cout << "The first set is a subset of the second." << endl;
+	}
+
+	result.resize(size(vec1) + size(vec2));
+	auto newEnd{ set_union(cbegin(vec1), cend(vec1), cbegin(vec2),
+		cend(vec2), begin(result)) };
+	DumpRange("The union is: ", begin(result), newEnd);
+
+	newEnd = set_intersection(cbegin(vec1), cend(vec1), cbegin(vec2),
+		cend(vec2), begin(result));
+	DumpRange("The intersection is: ", begin(result), newEnd);
+
+	newEnd = set_difference(cbegin(vec1), cend(vec1), cbegin(vec2),
+		cend(vec2), begin(result));
+	DumpRange("The difference is: ", begin(result), newEnd);
+
+	newEnd = set_symmetric_difference(cbegin(vec1), cend(vec1), cbegin(vec2),
+		cend(vec2), begin(result));
+	DumpRange("The symmetric difference is: ", begin(result), newEnd);
 }
