506f8b6dd90fe2893a4f97cc11757177ffd86bd9
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-28 16:51:40 +0900

    STUDY: 컴파일 시간 유리수

---
+++ 3부_전문가답게 C++ 코딩하기/22장_날짜와 시간 유틸리티/finite rational number/finite rational number.cpp
@@ -0,0 +1,26 @@
+﻿#include <ratio>
+#include <iostream>
+#include <format>
+
+using namespace std;
+
+int main()
+{
+	// 컴파일 시간 유리수 정의
+	using r1 = ratio<1, 60>;
+
+	// 분자와 분모를 구한다.
+	intmax_t num = r1::num;
+	intmax_t den = r1::den;
+	cout << format("1) r1 = {}/{}", num, den) << endl;
+
+	// 두 유리수를 더한다.
+	using r2 = ratio<1, 30>;
+	cout << format("2) r2 = {}/{}", r2::num, r2::den) << endl;
+	using result = ratio_add<r1, r2>::type;
+	cout << format("3) sum = {}/{}", result::num, result::den) << endl;
+
+	// 두 유리수를 비교한다.
+	using res = ratio_less<r2, r1>;
+	cout << format("4) r2 < r1: {}", res::value) << endl;
+}
