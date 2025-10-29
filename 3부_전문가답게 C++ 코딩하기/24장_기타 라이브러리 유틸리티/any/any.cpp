8cc4508b7340fd92cecbd24155c6cd600b0c3a41
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-29 14:54:50 +0900

    STUDY: any

---
+++ 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/any/any.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <any>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	any empty;
+	any anInt{ 3 };
+	any aString{ "An std::string."s };
+
+	cout << "empty.has_value = " << empty.has_value() << endl;
+	cout << "anInt.has_value = " << anInt.has_value() << endl;
+
+	cout << "anInt wrapped type = " << anInt.type().name() << endl;
+	cout << "aString wrapped type = " << aString.type().name() << endl;
+
+	int theInt{ any_cast<int>(anInt) };
+	cout << theInt << endl;
+	try {
+		int test{ any_cast<int>(aString) };
+		cout << test << endl;
+	} catch (const bad_any_cast& ex) {
+		cout << "Exception: " << ex.what() << endl;
+	}
+
+	any something{ 3 };
+	something = "An std::string"s;
+
+	vector<any> v;
+	v.push_back(42);
+	v.push_back("An std::string"s);
+
+	cout << any_cast<string>(v[1]) << endl;
+}
