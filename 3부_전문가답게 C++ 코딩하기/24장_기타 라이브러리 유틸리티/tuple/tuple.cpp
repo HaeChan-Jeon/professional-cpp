8b0297f278a528f73d8c2cb853616227322decd7
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-29 15:02:06 +0900

    STUDY: tuple1

---
+++ 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/tuple/tuple.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <utility>
+#include <tuple>
+#include <format>
+
+using namespace std;
+
+int main()
+{
+	pair<int, string> p1{ 16, "Hello World!" };
+	pair p2{ true, 0.123f };
+	cout << format("p1 = ({}, {})", p1.first, p1.second) << endl;
+	cout << format("p2 = ({}, {})", p2.first, p2.second) << endl;
+
+	using MyTuple = tuple<int, string, bool>;
+	MyTuple t1{ 16, "Test", true };
+
+	cout << format("t1 = ({}, {}, {})", get<0>(t1), get<1>(t1), get<2>(t1)) << endl;
+
+	cout << "Type of get<1>(t1) = " << typeid(get<1>(t1)).name() << endl;
+
+	cout << "Type of element with index 2 = "
+		<< typeid(tuple_element<2, MyTuple>::type).name() << endl;
+
+	cout << "String = " << get<string>(t1) << endl;
+}
