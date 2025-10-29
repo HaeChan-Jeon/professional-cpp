86f19ef3e727d6ac0e92c4fdf397a5a2b716fc52
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-29 14:19:42 +0900

    STUDY: 무작위수 엔진

---
+++ 3부_전문가답게 C++ 코딩하기/23장_무작위수 기능/random number engine/random number engine.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <random>
+
+using namespace std;
+
+using mt19937 = mersenne_twister_engine<uint_fast32_t, 32, 624, 397, 31,
+	0x9908b0df, 11, 0xffffffff, 7, 0x9d2c5680, 15, 0xefc60000, 18,
+	1812433252>;
+
+int main()
+{
+	random_device rnd;
+	cout << "Entropy: " << rnd.entropy() << endl;
+	cout << "Min value: " << rnd.min()
+		<< ", Max value: " << rnd.max() << endl;
+	cout << "Random number: " << rnd() << endl;
+}
