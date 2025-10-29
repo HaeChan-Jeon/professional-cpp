86d6aab2f1e4710fa8615fe0310790f743a5c7eb
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-29 14:38:20 +0900

    STUDY: 무작위수 분포

--- 3부_전문가답게 C++ 코딩하기/23장_무작위수 기능/random number engine/random number engine.cpp
+++ 3부_전문가답게 C++ 코딩하기/23장_무작위수 기능/random number engine/random number engine.cpp
@@ -2,29 +2,31 @@
 #include <random>
 #include <functional>
 #include <vector>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
-void fillVector(vector<int>& values, const auto& generator)
-{
-	generate(begin(values), end(values), generator);
-}
-
 int main()
 {
+	const unsigned int Start{ 1 };
+	const unsigned int End{ 99 };
+	const unsigned int Iterations{ 1'000'000 };
+
+	// 균등 메르센 트위스터
 	random_device seeder;
 	const auto seed{ seeder.entropy() ? seeder() : time(nullptr) };
 	mt19937 engine{ static_cast<mt19937::result_type>(seed) };
-
-	uniform_int_distribution<int> distribution{ 1, 99 };
-
-	cout << distribution(engine) << endl;
-
+	uniform_int_distribution<int> distribution{ Start, End };
 	auto generator{ bind(distribution, engine) };
-
-	vector<int> values(10);
-	//generate(begin(values), end(values), generator);
-	fillVector(values, generator);
-
-	for (auto i : values) { cout << i << " "; }
+	map<int, int> histogram;
+	for (unsigned int i{ 0 }; i < Iterations; ++i) {
+		int randomNumber{ generator() };
+		++(histogram[randomNumber]);
+	}
+	
+	ofstream of{ "res.csv" };
+	for (unsigned i{ Start }; i <= End; ++i) {
+		of << i << "," << histogram[i] << endl;
+	}
 }
