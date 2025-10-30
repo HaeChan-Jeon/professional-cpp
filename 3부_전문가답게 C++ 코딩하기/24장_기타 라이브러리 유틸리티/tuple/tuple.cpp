f65aa53554026e6bc37120da8f4bf580a6a77fab
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-30 10:25:49 +0900

    STUDY: make_from tuple

--- 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/tuple/tuple.cpp
+++ 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/tuple/tuple.cpp
@@ -10,29 +10,17 @@ using namespace std;
 class Foo
 {
 	public:
-		Foo(int i, string s, bool b) : m_int{ 1 }, m_str{ move(s) }, m_bool{ b }
+		Foo(string s, int i) : m_str{ move(s) }, m_int{ 1 }
 		{
 		}
-		auto operator<=>(const Foo& rhs) const = default;
 
 	private:
 		int m_int;
 		string m_str;
-		bool m_bool;
 };
 
 int main()
 {
-	tuple t1{ 123, "def"s };
-	tuple t2{ 123, "abc"s };
-	if (t1 < t2) {
-		cout << "t1 < t2" << endl;
-	} else {
-		cout << "t1 >= t2" << endl;
-	}
-
-	Foo f1{ 42, "Hello", false };
-	Foo f2{ 42, "World", false };
-	cout << (f1 < f2) << endl;
-	cout << (f2 < f1) << endl;
+	tuple myTuple{ "Hello world", 42 };
+	auto foo{ make_from_tuple<Foo>(myTuple) };
 }
