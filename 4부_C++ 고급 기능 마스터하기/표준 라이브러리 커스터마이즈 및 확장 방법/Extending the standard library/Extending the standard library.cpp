a542333209f0098cfc5eb81e4e0f6c89b74ced1c
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-30 13:40:22 +0900

    STUDY: find_all()

---
+++ 4부_C++ 고급 기능 마스터하기/표준 라이브러리 커스터마이즈 및 확장 방법/Extending the standard library/Extending the standard library.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <format>
+
+using namespace std;
+
+template <typename InputIterator, typename OutputIterator, typename Predicate>
+OutputIterator find_all(InputIterator first, InputIterator last,
+    OutputIterator dest, Predicate pred)
+{
+    while (first != last) {
+        if (pred(*first)) {
+            *dest = first;
+            ++dest;
+        }
+        ++first;
+    }
+    return dest;
+}
+
+int main()
+{
+    vector<int> vec{ 3, 4, 5, 4, 5, 6, 5, 8 };
+    vector<vector<int>::iterator> matches;
+
+    find_all(begin(vec), end(vec), back_inserter(matches),
+        [](int i) {return i == 5;});
+
+    cout << format("Found {} matching elements: ", matches.size()) << endl;
+    for (const auto& it : matches) {
+        cout << format("{} at position {}", *it, distance(begin(vec), it)) << endl;
+    }
+}
