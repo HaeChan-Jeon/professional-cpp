f2857f682f4d14c9f19b532aafdac89b1229948e
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-30 11:43:51 +0900

    STUDY: 할당자

---
+++ 4부_C++ 고급 기능 마스터하기/표준 라이브러리 커스터마이즈 및 확장 방법/allocator/allocator.cpp
@@ -0,0 +1,19 @@
+﻿#include <scoped_allocator>
+
+using namespace std;
+
+class MyClass {};
+int main()
+{
+	// 사용할 할당자를 생성한다.
+	allocator<MyClass> alloc;
+	// MyClass 인스턴스 한 개에 대한 초기화되지 않은 메모리 블록을 할당한다.
+	auto* memory{ alloc.allocate(1) };
+	// new 연산자를 이용하여 방금 생성한 블록에 MyClass 객체를 생성한다.
+	new(memory) MyClass;
+	// MyClass 인스턴스를 제거한다.
+	destroy_at(memory);
+	// 메모리 블록을 해제한다.
+	alloc.deallocate(memory, 1);
+	memory = nullptr;
+}
