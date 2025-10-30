4f870803aa41e339bb1b503179863e570ba6a463
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-30 10:15:42 +0900

    STUDY: tuple 연결

--- 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/tuple/tuple.cpp
+++ 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/tuple/tuple.cpp
@@ -9,21 +9,6 @@ using namespace std;
 int main()
 {
 	tuple t1{ 16, "Test"s, true };
-	auto [i, str, b] { t1 };
-	cout << format("Decomposed: i = {}, str = \"{}\", b = {}", i, str, b) << endl;
-
-	auto& [i2, str2, b2] { t1 };
-	i2 *= 2;
-	str2 = "Hello World";
-	b2 = !b2;
-	cout << format("Decomposed: i = {}, str = \"{}\", b = {}", i2, str2, b2) << endl;
-
-	int i3{ 0 };
-	string str3;
-	bool b3{ false };
-	cout << format("Before: i = {}, str = \"{}\", b = {}", i3, str3, b3) << endl;
-	tie(i3, str3, b3) = t1;
-	cout << format("After: i = {}, str = \"{}\", b = {}", i3, str3, b3) << endl;
-
-	tie(i, ignore, b) = t1;
+	tuple t2{ 3.14, "string 2"s };
+	auto t3{ tuple_cat(t1, t2) };
 }
