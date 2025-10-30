455a1b018f9f9ca22c094b848009dc93153dfb1e
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-30 10:22:37 +0900

    STUDY: tuple 비교

--- 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/tuple/tuple.cpp
+++ 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/tuple/tuple.cpp
@@ -3,12 +3,36 @@
 #include <tuple>
 #include <format>
 #include <functional>
+#include <compare>
 
 using namespace std;
 
+class Foo
+{
+	public:
+		Foo(int i, string s, bool b) : m_int{ 1 }, m_str{ move(s) }, m_bool{ b }
+		{
+		}
+		auto operator<=>(const Foo& rhs) const = default;
+
+	private:
+		int m_int;
+		string m_str;
+		bool m_bool;
+};
+
 int main()
 {
-	tuple t1{ 16, "Test"s, true };
-	tuple t2{ 3.14, "string 2"s };
-	auto t3{ tuple_cat(t1, t2) };
+	tuple t1{ 123, "def"s };
+	tuple t2{ 123, "abc"s };
+	if (t1 < t2) {
+		cout << "t1 < t2" << endl;
+	} else {
+		cout << "t1 >= t2" << endl;
+	}
+
+	Foo f1{ 42, "Hello", false };
+	Foo f2{ 42, "World", false };
+	cout << (f1 < f2) << endl;
+	cout << (f2 < f1) << endl;
 }
