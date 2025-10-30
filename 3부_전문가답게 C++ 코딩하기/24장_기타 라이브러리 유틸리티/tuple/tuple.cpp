559feb353d253113cec1511ecb4c1aac4184d53e
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-30 10:11:25 +0900

    STUDY: 구조적 바인딩

--- 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/tuple/tuple.cpp
+++ 3부_전문가답게 C++ 코딩하기/24장_기타 라이브러리 유틸리티/tuple/tuple.cpp
@@ -8,36 +8,13 @@ using namespace std;
 
 int main()
 {
-	pair<int, string> p1{ 16, "Hello World!" };
-	pair p2{ true, 0.123f };
-	cout << format("p1 = ({}, {})", p1.first, p1.second) << endl;
-	cout << format("p2 = ({}, {})", p2.first, p2.second) << endl;
+	tuple t1{ 16, "Test"s, true };
+	auto [i, str, b] { t1 };
+	cout << format("Decomposed: i = {}, str = \"{}\", b = {}", i, str, b) << endl;
 
-	using MyTuple = tuple<int, string, bool>;
-	MyTuple t1{ 16, "Test", true };
-
-	cout << format("t1 = ({}, {}, {})", get<0>(t1), get<1>(t1), get<2>(t1)) << endl;
-
-	cout << "Type of get<1>(t1) = " << typeid(get<1>(t1)).name() << endl;
-
-	cout << "Type of element with index 2 = "
-		<< typeid(tuple_element<2, MyTuple>::type).name() << endl;
-
-	cout << "String = " << get<string>(t1) << endl;
-
-	cout << "Tuple Size = " << tuple_size<MyTuple>::value << endl;
-	
-	cout << "Tuple Size = " << tuple_size<decltype(t1)>::value << endl;
-
-	//tuple t1{ 16, "Test"s, true };
-
-	double d{ 3.14 };
-	string str1{ "Test" };
-	tuple t2{ 16, ref(d), cref(d), ref(str1) };
-
-	cout << "d = " << d << endl;
-	get<1>(t2) *= 2;
-	cout << "d = " << d << endl;
-
-	auto t2{ make_tuple(16, ref(d), cref(d), ref(str1)) };
+	auto& [i2, str2, b2] { t1 };
+	i2 *= 2;
+	str2 = "Hello World";
+	b2 = !b2;
+	cout << format("Decomposed: i = {}, str = \"{}\", b = {}", i2, str2, b2) << endl;
 }
