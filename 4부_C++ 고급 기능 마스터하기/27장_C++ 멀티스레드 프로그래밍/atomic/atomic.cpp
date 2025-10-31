f7f31caa0f57fd63b80136716b661d99c8eb01b9
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-31 10:38:36 +0900

    STUDY: 아토믹 연산 라이브러리

---
+++ 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/atomic/atomic.cpp
@@ -0,0 +1,18 @@
+﻿#include <atomic>
+#include <iostream>
+
+using namespace std;
+
+class Foo { private: int m_array[123]; };
+class Bar { private: int m_int; };
+
+int main()
+{
+	atomic<Foo> f;
+	// 결과: 1 0
+	cout << is_trivially_copyable_v<Foo> << " " << f.is_lock_free() << endl;
+
+	atomic<Bar> b;
+	// 결과: 1 1
+	cout << is_trivially_copyable_v<Bar> << " " << b.is_lock_free() << endl;
+}
