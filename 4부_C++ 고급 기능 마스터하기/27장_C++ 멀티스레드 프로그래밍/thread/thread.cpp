197749539a130716f4e6a69a43919cc3791f8bc5
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-31 09:10:32 +0900

    STUDY: 람다 표현식으로 스레드 만들기

--- 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/thread/thread.cpp
+++ 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/thread/thread.cpp
@@ -3,34 +3,14 @@
 
 using namespace std;
 
-class Counter
-{
-	public:
-		Counter(int id, int numIterations)
-			: m_id{ id }, m_numIterations{ numIterations } {
-		}
-
-		void operator()() const
-		{
-			for (int i{ 0 }; i < m_numIterations; ++i) {
-				cout << "Counter " << m_id << " has value" << i << endl;
-			}
-		}
-	private:
-		int m_id;
-		int m_numIterations;
-};
-
 int main()
 {
-	// 균일 초기화를 사용하는 방법
-	thread t1{ Counter{1, 20} };
-
-	// 일반 변수처럼 네임드 인스턴스로 초기화하는 방법
-	Counter c{ 2, 12 };
-	thread t2{ c };
-
-	// 두 스레드가 모두 마칠 때까지 기다린다.
+	int id{ 1 };
+	int numIterations{ 5 };
+	thread t1{ [id, numIterations] {
+		for (int i{ 0 }; i < numIterations; ++i) {
+			cout << "Counter " << id << " has value " << i << endl;
+		}
+	} };
 	t1.join();
-	t2.join();
 }
