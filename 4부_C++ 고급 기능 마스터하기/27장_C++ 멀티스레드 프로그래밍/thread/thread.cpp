82823a24f0952e7af923f9c42c26944a99e808a2
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-31 09:13:25 +0900

    STUDY: 멤버 함수로 스레드 만들기

--- 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/thread/thread.cpp
+++ 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/thread/thread.cpp
@@ -3,14 +3,18 @@
 
 using namespace std;
 
+class Request
+{
+	public:
+		Request(int id) : m_id{ id } { }
+		void process() { cout << "Processing request " << m_id << endl; }
+	private:
+		int m_id;
+};
+
 int main()
 {
-	int id{ 1 };
-	int numIterations{ 5 };
-	thread t1{ [id, numIterations] {
-		for (int i{ 0 }; i < numIterations; ++i) {
-			cout << "Counter " << id << " has value " << i << endl;
-		}
-	} };
-	t1.join();
+	Request req{ 100 };
+	thread t{ &Request::process, &req };
+	t.join();
 }
