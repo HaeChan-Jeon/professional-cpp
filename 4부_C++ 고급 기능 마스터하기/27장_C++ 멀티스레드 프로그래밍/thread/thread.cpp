8d6e2f8f473470c005873bbbc78c6b3111b2341b
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-10-31 10:01:06 +0900

    STUDY: 자동으로 스레드 조인하기

--- 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/thread/thread.cpp
+++ 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/thread/thread.cpp
@@ -4,18 +4,13 @@
 
 using namespace std;
 
-int k;
-thread_local int n;
-
-void threadFunction(int id)
-{
-	cout << format("Thread {}: k={}, n={}\n", id, k, n);
-	++n;
-	++k;
-}
+jthread job{ [](stop_token token) {
+	while (!token.stop_requested()) {
+		// ...
+	}
+} };
 
 int main()
 {
-	thread t1{ threadFunction, 1 }; t1.join();
-	thread t2{ threadFunction, 2 }; t2.join();
+	job.request_stop();
 }
