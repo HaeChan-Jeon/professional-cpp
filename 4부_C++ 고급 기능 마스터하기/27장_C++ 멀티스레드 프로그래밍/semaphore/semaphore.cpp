688950be84af8e507a69b8534f130f1ad276928e
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-11-03 11:11:07 +0900

    STUDY: 세마포어

---
+++ 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/semaphore/semaphore.cpp
@@ -0,0 +1,18 @@
+﻿#include <semaphore>
+#include <vector>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+	counting_semaphore semaphore{ 4 };
+	vector<jthread> threads;
+	for (int i{0}; i < 10; ++i) {
+		threads.push_back(jthread{ [&semaphore] {
+			semaphore.acquire();
+
+			semaphore.release();
+		} });
+	}
+}
