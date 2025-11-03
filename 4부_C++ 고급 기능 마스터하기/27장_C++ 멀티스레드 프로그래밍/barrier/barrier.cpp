121ed7a9e6388237357863ac18757d56d57aed3e
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-11-03 11:06:23 +0900

    STUDY: 배리어

---
+++ 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/barrier/barrier.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <barrier>
+#include <vector>
+#include <thread>
+
+using namespace std;
+
+void completionFunction() noexcept {}
+
+int main()
+{
+	const size_t numberOfThreads{ 4 };
+	barrier barrierPoint{ numberOfThreads, completionFunction };
+	vector<jthread> threads;
+
+	for (int i{ 0 }; i < numberOfThreads; ++i) {
+		threads.push_back(jthread{ [&barrierPoint](stop_token token) {
+			while (!token.stop_requested()) {
+				// 계산을 수행한다.
+
+				// 다른 스레드와 동기화한다.
+				barrierPoint.arrive_and_wait();
+			}
+		} });
+	}
+}
