c40549f88afc57fe1d87f0814052c21b86a3a898
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-11-03 11:33:07 +0900

    STUDY: 퓨처 익셉션 처리

--- 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/future/future.cpp
+++ 4부_C++ 고급 기능 마스터하기/27장_C++ 멀티스레드 프로그래밍/future/future.cpp
@@ -4,15 +4,23 @@
 
 using namespace std;
 
-int calculate() { return 123; }
+int calculate() {
+    throw runtime_error{ "Exception throw from calculate()." };
+}
 
 int main()
 {
-    auto myFuture{ async(calculate) };
+    // 강제로 비동기식으로 실행하도록 launch::async 정책을 지정한다.
+    auto myFuture{ async(launch::async, calculate) };
 
     // 다른 작업을 수행한다.
 
     // 결과를 가져온다.
-    int result{ myFuture.get() };
-    cout << "Result: " << result << endl;
+    try {
+        int result{ myFuture.get() };
+        cout << result << endl;
+    }
+    catch (const exception& ex) {
+        cout << "Caught exception: " << ex.what() << endl;
+    }
 }
