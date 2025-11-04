55277875052529a896592461213415df78a8e987
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-11-04 10:17:54 +0900

    STUDY: 레퍼런스 전달과 값 전달

---
+++ 5부_C++ 소프트웨어 공학/29장_효율적인 C++ 코드 작성법/Language-level efficiency/Language-level efficiency.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Person
+{
+    public:
+        Person() = default;
+        Person(string firstName, string lastName, int age)
+            : m_firstName{ move(firstName) }, m_lastName{ move(lastName) }
+            , m_age { age } { }
+        virtual ~Person() = default;
+
+        const string& getFirstName() const { return m_firstName; }
+        const string& getLastName() const { return m_lastName; }
+        int getAge() const { return m_age; }
+    private:
+        string m_firstName, m_lastName;
+        int m_age{ 0 };
+};
+
+//void processPerson(Person& p) {/* Person 타입 객체를 처리한다. */ }
+void processPerson(const Person& p) {/* Person 타입 객체를 처리한다. */}
+
+int main()
+{
+    Person me{ "Marc", "Gregoire", 42 };
+    processPerson(me);
+}
