7bfc632878dca435f9239640e684fd6de58ae5cd
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-11-14 15:12:31 +0900

    STUDY: 익셉션 던지고 받기

--- 5부_C++ 소프트웨어 공학/32장_설계 기법과 프레임워크/design technique/design technique.cpp
+++ 5부_C++ 소프트웨어 공학/32장_설계 기법과 프레임워크/design technique/design technique.cpp
@@ -1,20 +1,23 @@
-﻿#include <iostream>
-#include <vector>
-#include <format>
-#include <algorithm>
+﻿#include <stdexcept>
+#include <iostream>
 
 using namespace std;
 
+void throwIf(bool throwIt)
+{
+	if (throwIt) {
+		throw runtime_error{ "Here's my exception" };
+	}
+}
+
 int main()
 {
-	vector values{ 1,2,3,4,5,6,7,8,9 };
-	int callCount{ 0 };
-	auto count{ count_if(begin(values), end(values),
-		[&callCount](int value) {
-			++callCount;
-			return value % 2 == 0;
-		})
-	};
-	cout << format("There are {} even elements in the vector.", count) << endl;
-	cout << format("Lambda was called {} times.", callCount) << endl;
+	try {
+		throwIf(false);
+		throwIf(true);
+	}
+	catch (const runtime_error& e) {
+		cerr << "Caught exception: " << e.what() << endl;
+		return 1;
+	}
 }
